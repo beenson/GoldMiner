daf13be298695b6bd03ad0f12ab6d5a16c6ac314
108590027 <t108590027@ntut.org.tw>
2021-04-30 11:14:43 +0800

    Mystery bag has effects

--- demo/OOPLab_Sample/game_sample/js/oldman.js
+++ demo/OOPLab_Sample/game_sample/js/oldman.js
@@ -61,6 +61,10 @@ var Oldman = Framework.exClass({
             this.bomb.scale = 0.3;
             this.baseScene.attach(this.bomb)
         }
+
+        this.hasBook = this.effect.indexOf("book") != -1;
+        this.hasBottle = this.effect.indexOf("bottle") != -1;
+        this.hasClover = this.effect.indexOf("clover") != -1;
         //----------item effect end----------
     },
 
@@ -89,23 +93,31 @@ var Oldman = Framework.exClass({
             this.audio.stopAll();
         if(this.grabbing) {
             var value = this.grabbing.value;
-            if(value == -1) {
+            if(value == -1) {       //mystery bag
                 value = Math.floor(Math.random() * 900) + 1;
+                if(this.hasClover){ //clover lucky boost
+                    value = value + 200;
+                }
             }
             this.grabbing.detach();
             if(value > 800) {
-                if(Math.random() >= 0.5) {
-                    //firecracker
-                }else {
-                    //streng
+                if(Math.random() <= 0.5) {
+                    if(!this.hasBomb){  //firecracker
+                        console.log("Bomb-Mystery")
+                        this.hasBomb = true;
+                        this.baseScene.attach(this.bomb);
+                    }
+                }else {                 //potion effect
+                        console.log("Potion-Mystery")
+                    this.powerAdd = 1.2;
                 }
                 this.grabbing = undefined;
             }else {
-                if((this.grabbing.family === "stone") && (this.effect.indexOf("book") != -1)){
+                if((this.grabbing.family === "stone") && this.hasBook){
                     console.log(this.effect.indexOf("book"));
                     value = value * 3;
                 }
-                else if((this.grabbing.family === "diamond") && (this.effect.indexOf("bottle") != -1)) {
+                else if((this.grabbing.family === "diamond") && this.hasBottle) {
                     value = value + 300;
                 }
                 this.grabbing = undefined;
@@ -145,6 +157,7 @@ var Oldman = Framework.exClass({
         if(this.status === "pulling" && this.hasBomb && this.grabbing){
             this.grabbing.detach();
             this.baseScene.detach(this.bomb)
+            this.pullSpeed = 15;
             this.hasBomb = false;
             this.grabbing = undefined;
         }
