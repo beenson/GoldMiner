7ff1ad438f1f73a24631186088024731546866c2
108590027 <t108590027@ntut.org.tw>
2021-06-18 11:45:08 +0800

    delete files

--- demo/OOPLab_Sample/game_sample/js/loadGame.js
+++ demo/OOPLab_Sample/game_sample/js/loadGame.js
@@ -50,12 +50,9 @@ var loadGameEnd;
         { src: 'game_sample/js/object.js', lookFor: 'Object' },
         { src: 'game_sample/js/shopItem.js', lookFor: 'shopItem' },
         { src: 'game_sample/js/myMenu.js', lookFor: 'MyMenu' },
-        { src: 'game_sample/js/character.js', lookFor: 'Character' },
-        { src: 'game_sample/js/Practice.js', lookFor: 'Practice' },
         { src: 'game_sample/js/GameMap.js', lookFor: 'GameMap' },
         { src: 'game_sample/js/myGameLevel1.js', lookFor: 'MyGame' },
         { src: 'game_sample/js/shop.js', lookFor: 'Shop' },
-        { src: 'game_sample/js/myGameLevel2.js', lookFor: 'MyGame2' },
         { src: 'game_sample/js/mainGame.js'}
     ]
 
